ed06f2085e141ef666c27956d88cf6a0ccaffbe5
Chameleon Cloud User <cc@hurryup.novalocal>
2019-07-29 17:27:48 +0000

    Add standalone mode

---
+++ bin/xapian-pure/client_standalone.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/../deps/configs.sh
+
+NSERVERS=1
+QPS=1000
+WARMUPREQS=1000
+REQUESTS=500000
+
+TBENCH_QPS=${QPS} TBENCH_MINSLEEPNS=100000 TBENCH_CLIENT_THREADS=2 \
+    TBENCH_SERVER=129.114.108.163 TBENCH_TERMS_FILE=${DATA_ROOT}/xapian/terms.in \
+    chrt -r 99 ./xapian_networked_client &
+
+echo $! > client.pid
+
+
+
+wait $(cat client.pid)
+
+# Clean up
+./kill_standalone.sh
+rm client.pid
