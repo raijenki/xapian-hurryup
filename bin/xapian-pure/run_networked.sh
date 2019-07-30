b3d1956616bb939aab7ddba4792af23ad0636ba1
Chameleon Cloud User <cc@hurryup.novalocal>
2019-07-30 21:06:27 +0000

    Results

--- bin/xapian-pure/run_networked.sh
+++ bin/xapian-pure/run_networked.sh
@@ -3,19 +3,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/../deps/configs.sh
 
-NSERVERS=4
-QPS=12000
+NSERVERS=1
+QPS=400
 WARMUPREQS=1000
-REQUESTS=720000
+REQUESTS=80000
 
 TBENCH_MAXREQS=${REQUESTS} TBENCH_WARMUPREQS=${WARMUPREQS} \
     chrt -r 99 ./xapian_networked_server -n ${NSERVERS} -d ${DATA_ROOT}/xapian/wiki \
     -r 1000000000 &
 echo $! > server.pid
-taskset -apc 12,14,16,18 $(cat server.pid)
+taskset -apc 12 $(cat server.pid)
 sleep 5 # Wait for server to come up
 
-TBENCH_QPS=${QPS} TBENCH_MINSLEEPNS=10000 TBENCH_CLIENT_THREADS=10 \
+TBENCH_QPS=${QPS} TBENCH_MINSLEEPNS=10000 TBENCH_CLIENT_THREADS=2 \
     TBENCH_TERMS_FILE=${DATA_ROOT}/xapian/terms.in \
     chrt -r 99 ./xapian_networked_client &
 
--- raw-results/03/Readme.md
+++ raw-results/03/Readme.md
@@ -1,4 +1,10 @@
 Standalone // Análise de Frquencia x Tempo de Latencia de Cauda
+Parametrizacao: QPS=1000, WUR=1000, R=80k
+
 01.txt - 1.2 GHz
 95% = 481876.845 ms
 99% = 501412.498 ms
+
+02.txt - 1.4Ghz
+
+03.txt - 
