f409156da78c262d35799e2053cbd0fbe7312997
raijenki <daniel@kuriate.com.br>
2019-10-12 21:55:27 +0000

    yeah

--- raw-results/08/convert.sh
+++ raw-results/08/convert.sh
@@ -1,7 +1,7 @@
-for counter in 1 2 10 15 17 20 28 31 32 33 39
+for counter in 2 6 30 32 33 37
 #was having lol errors with {1..30..1}
 	do
 		echo "File: $counter"
-		python parselats.py hurryup-start@1.4ghz-10servers-$counter.bin
+		python parselats.py pure-start@1.4ghz-10servers-$counter.bin
 	done
 mv *.txt txt
