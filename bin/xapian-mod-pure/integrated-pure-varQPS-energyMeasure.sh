e75b10b2430e8fed4d41e18b919d4b789bf35a5d
Daniel Araujo <daniel@kuriate.com.br>
2020-01-28 17:42:40 +0000

    Bin folder

--- bin/xapian-mod-pure/integrated-pure-varQPS-energyMeasure.sh
+++ bin/xapian-mod-pure/integrated-pure-varQPS-energyMeasure.sh
@@ -3,33 +3,48 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/../deps/configs.sh
 
-NSERVERS=12
+NSERVERS=7
 PROCESSTIME=60
 WARMUPTIME=5
 
-for queryPerSecond in {4800..4800..1200}
+for cpus in cpu14 cpu16 cpu18 cpu20 cpu22 
 do
-	REQUESTS=$((queryPerSecond * PROCESSTIME))
-	WARMUPREQS=$((WARMUPTIME * queryPerSecond))
-	echo "Run, qps, Difference" > energy-pure.txt
-	for counter in {1..1..1}
+	sudo echo 0 | sudo tee /sys/devices/system/cpu/${cpus}/online
+done
+
+for cpus in cpu12 cpu14 cpu16 cpu18 cpu20 cpu22
+do
+	sudo echo 1 | sudo tee /sys/devices/system/cpu/${cpus}/online
+
+	for frequencyStep in 1.0GHz 1.2GHz 1.3GHz 1.5GHz 1.7GHz 1.9GHz 2.1GHz 2.3GHz 2.5GHz
 	do
-	cat /sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state > /home/cc/hurryup-dvfs/raw-results/ondemand-mod/tis-pure-governor@ondemandMod-12servers-${counter}-${queryPerSecond}-start.txt
-	energyStart=$(cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj)
+		sudo cpupower frequency-set -f ${frequencyStep}
+
+		for queryPerSecond in {1200..6000..2400}
+		do
+		REQUESTS=$((queryPerSecond * PROCESSTIME))
+		WARMUPREQS=$((WARMUPTIME * queryPerSecond))
+		echo "Run, qps, Difference" > energy-pure-${cpus}.txt
+			for counter in {1..3..1}
+			do
+			cat /sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state > /home/cc/hurryup-dvfs/results/core-augmentation/tis-pure-governor@userspace-${frequencyStep}-12servers-${counter}-${queryPerSecond}-start-${cpus}.txt
+			energyStart=$(cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj)
 	TBENCH_QPS=${queryPerSecond} TBENCH_CLIENT_THREADS=12 TBENCH_MAXREQS=${REQUESTS} TBENCH_WARMUPREQS=${WARMUPREQS} \
 		TBENCH_MINSLEEPNS=100000 TBENCH_TERMS_FILE=${DATA_ROOT}/xapian/terms.in \
        		numactl --cpunodebind=1 ./xapian_integrated -n ${NSERVERS} -d ${DATA_ROOT}/xapian/wiki -r 1000000000
-	echo $! > server.pid
-	energyEnd=$(cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj)
-	cat /sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state > /home/cc/hurryup-dvfs/raw-results/ondemand-mod/tis-pure-governor@ondemandMod-12servers-${counter}-${queryPerSecond}-end.txt
-	consumption=$((energyEnd - energyStart))
-	echo "$counter, $queryPerSecond, $consumption" >> energy-pure.txt
-	python parselats.py lats.bin
-	mv lats.txt ../../raw-results/ondemand-mod/pure-governor@ondemandMod-12servers-${counter}-${queryPerSecond}.txt
-	rm server.pid
+			echo $! > server.pid
+			energyEnd=$(cat /sys/class/powercap/intel-rapl/intel-rapl\:0/energy_uj)
+			cat /sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state > /home/cc/hurryup-dvfs/results/core-augmentation/tis-pure-governor@userspace-${frequencyStep}-12servers-${counter}-${queryPerSecond}-end-${cpus}.txt
+			consumption=$((energyEnd - energyStart))
+			echo "$counter, $queryPerSecond, $consumption" >> energy-pure-${cpus}.txt
+			python parselats.py lats.bin
+			mv lats.txt ../../results/core-augmentation/pure-governor@userspace-${frequencyStep}-12servers-${counter}-${queryPerSecond}-${cpus}.txt
+			rm server.pid
+			done
+		mv energy-pure-${cpus}.txt ../../results/core-augmentation/energy-pure-governor@userspace-${frequencyStep}-12servers-${queryPerSecond}-${cpus}.txt
+		done
 	done
-	mv energy-pure.txt ../../raw-results/ondemand-mod/energy-pure-governor@ondemandMod-12servers-${queryPerSecond}.txt
+	NSERVERS=$((NSERVERS+1))
 done
-
 #taskset -apc 12 $(cat server.pid)
 
---
+++ bin/xapian-mod-pure/test.sh
@@ -0,0 +1,1 @@
+ for i in 1.0GHZ 2.0GHz 3.2GHz 4.5GHz 5.6GHz; do echo "counter: $i"; done
